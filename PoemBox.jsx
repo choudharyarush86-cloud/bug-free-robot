057522dfd5e54eb71a235416b430f2018c58fe93
Arush Choudhary <choudharyarush86@gmail.com>
2026-05-18 12:45:04 +0530

    Update PoemBox.jsx

--- PoemBox.jsx
+++ PoemBox.jsx
@@ -1,4 +1,11 @@
 import React from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const api = import.meta
+const api = import.meta.env.local.VITE_API_KEY
+export default function PoemBox() {
+  function fetchPoem() {
+    try {
+      const genAI = new GoogleGenerativeAI(api)
+    }
+  }
+}
